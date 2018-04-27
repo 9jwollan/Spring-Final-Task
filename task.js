04872fa487d87e91a9152fc738b3869d6a9e7c2d
9jwollan <31480922+9jwollan@users.noreply.github.com>
2018-04-27 09:16:26 -0500

    Add files via upload

--- task.js
+++ task.js
@@ -4,18 +4,26 @@ const TaskExport = function(){
 
     //returns a task object, similar to pizza
     function createTask(v,t){
-        
+      return{
+        size;s
+        cost;c
+      }
     }
-
+let p1=new product
     //returns a random integer between 0 and n inclusive
     function rInt(n){
-
+math.random()
+math.random = math.random * n
+math.round(math.random,n)
     }
     //returns a list of task objects.
     //You must reference the function above to create them each time
     //make all times range from 0 to 15 and all values range from 0 to 100
     function Factory(n){
-
+let list = []
+  for();
+    let task = new CreateTask(,);
+    return list;
     }
 
     //do not change anything below there!
